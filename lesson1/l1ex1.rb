4dcda92b0c4f8da0d1f37728346250b4366e07cd
Evgeny Prygunov <prygunov.ea@gmail.com>
2018-01-26 18:33:53 +0300

    Lesson 1 rev4

--- lesson1/l1ex1.rb
+++ lesson1/l1ex1.rb
@@ -4,8 +4,10 @@ name = gets.chomp
 puts 'Какой у тебя рост (в см)?'
 height = gets.chomp.to_i
 
-if height - 110 > 0
-  puts "#{name}! Ваш идеальный вес #{height - 110} кг."
+ideal_weight = height - 110
+
+if ideal_weight > 0
+  puts "#{name}! Ваш идеальный вес #{ideal_weight} кг."
 elsif
   puts "#{name}! Ваш вес уже оптимальный."
 end

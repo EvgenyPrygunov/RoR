dda16e4f5f0f5ef9a2878e5316b3db2556de8e0f
Evgeny Prygunov <prygunov.ea@gmail.com>
2018-01-31 18:47:06 +0300

    Lesson 2 rev2

--- lesson2/l2ex4.rb
+++ lesson2/l2ex4.rb
@@ -1,17 +1,4 @@
-letters = ('a'..'z').to_a
+letters = ('a'..'z')
+vovels = ['a', 'e', 'i', 'o', 'u']
 
-i = 0
-alphabet = Hash.new
-
-while i < 26
-  alphabet[letters[i]] = i + 1
-  i += 1
-end
-
-alphabet.each_with_index do |(key, value), index |
-  puts "#{key}: #{value}." if index == 0
-  puts "#{key}: #{value}." if index == 4
-  puts "#{key}: #{value}." if index == 8
-  puts "#{key}: #{value}." if index == 14
-  puts "#{key}: #{value}." if index == 20
-end
+letters.each.with_index(1) { |l, i| puts "#{l}: #{i}." if vovels.include?(l) }
